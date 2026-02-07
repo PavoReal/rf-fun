9633de8696a9e77d80630a738e8996a31a59d697
Garrison Peacock <garrison.peacock@gmail.com>
2026-02-06 17:50:20 -0800

    Starting to play with rx callback

--- src/main.zig
+++ src/main.zig
@@ -13,12 +13,51 @@ fn khz(val: comptime_float) comptime_int {
     return @intFromFloat(val * 1e3);
 }
 
-fn rxCallback(_: hackrf.Transfer, _: void) hackrf.StreamAction {
-    return .stop;
+fn kb(val: comptime_float) comptime_int {
+    return @intFromFloat(val * 1e3);
+}
+
+fn mb(val: comptime_float) comptime_int {
+    return @intFromFloat(val * 1e3);
+}
+
+fn gb(val: comptime_float) comptime_int {
+    return @intFromFloat(val * 1e3);
+}
+
+const CallbackState = struct {
+    should_stop: bool = true,
+    bytes_transfered: u64 = 0,
+};
+
+fn rxCallback(trans: hackrf.Transfer, state: *CallbackState) hackrf.StreamAction {
+    const valid_length = trans.validLength();
+
+    std.log.debug("Ready with {d} Mb", .{valid_length / 1000});
+    state.bytes_transfered += trans.validLength();
+
+    // I need to get array of datatypes out
+
+    if (state.should_stop) return .stop;
+    return .@"continue";
 }
 
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
+
+    var stdin_buf: [4096]u8 = undefined;
+    var stdout_buf: [4096]u8 = undefined;
+
+    var stdin_rdr = std.Io.File.stdin().reader(io, &stdin_buf);
+    var stdout_wtr = std.Io.File.stdout().writer(io, &stdout_buf);
+
+    const stdin = &stdin_rdr.interface;
+    const stdout = &stdout_wtr.interface;
+
+    // For dev cycle, clear stdout. Good with
+    // zig build run --watch
+    try stdout.writeAll("\x1B[2J\x1B[3J\x1B[H");
+    try stdout.flush();
 
     try hackrf.init();
     defer hackrf.deinit() catch {};
@@ -44,26 +83,24 @@ pub fn main(init: std.process.Init) !void {
     try device.setSampleRate(mhz(10));
     try device.setFreq(ghz(0.9));
     try device.setAmpEnable(true);
+    
+    std.log.debug("hackrf config done", .{});
 
-    try device.startRx(void, rxCallback, {});
+    var rx_state = CallbackState{};
+    try device.startRx(*CallbackState, rxCallback, &rx_state);
 
-    var stdin_buf: [4096]u8 = undefined;
-    var stdin_rdr = std.Io.File.stdin().reader(io, &stdin_buf);
-
-    const stdin = &stdin_rdr.interface;
-
-    std.log.info("Press q to exit", .{});
+    std.log.info("Press q<Enter> to exit", .{});
     while (stdin.takeDelimiterExclusive('\n')) |line| {
         stdin.toss(1); // toss the delimiter
 
         if (std.mem.eql(u8, line, "q")) {
-            return;
+
+            break;
         }
     } else |err| switch (err) {
         error.EndOfStream => {},
         error.StreamTooLong => return err,
         error.ReadFailed => return err,
     }
-
-    std.log.debug("hackrf config done", .{});
 }
+
